8bd9b38b054b199b437abe92c779f3beda83a22f
Jozef Mlich <jmlich83@gmail.com>
2022-04-16 19:28:45 +0200

    lupdate

--- translations/glacier-music.ts
+++ translations/glacier-music.ts
@@ -5,6 +5,7 @@
     <name>EditTrackPage</name>
     <message>
         <location filename="../src/qml/pages/EditTrackPage.qml" line="17"/>
+        <location filename="../src/qml/pages/EditTrackPage.qml" line="17"/>
         <source>Edit track info</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12,6 +13,7 @@
 <context>
     <name>LibraryPage</name>
     <message>
+        <location filename="../src/qml/pages/LibraryPage.qml" line="15"/>
         <location filename="../src/qml/pages/LibraryPage.qml" line="15"/>
         <source>Library</source>
         <translation type="unfinished"></translation>
@@ -21,6 +23,7 @@
     <name>PlayListPage</name>
     <message>
         <location filename="../src/qml/pages/PlayListPage.qml" line="14"/>
+        <location filename="../src/qml/pages/PlayListPage.qml" line="14"/>
         <source>Playlist</source>
         <translation type="unfinished"></translation>
     </message>
@@ -29,7 +32,20 @@
     <name>PlayerPage</name>
     <message>
         <location filename="../src/qml/pages/PlayerPage.qml" line="18"/>
+        <location filename="../src/qml/pages/PlayerPage.qml" line="18"/>
         <source>Music</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/PlayerPage.qml" line="70"/>
+        <location filename="../src/qml/pages/PlayerPage.qml" line="70"/>
+        <source>Unknown track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/PlayerPage.qml" line="84"/>
+        <location filename="../src/qml/pages/PlayerPage.qml" line="84"/>
+        <source>Unknown artist</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -45,15 +61,36 @@
     <name>SettingsPage</name>
     <message>
         <location filename="../src/qml/pages/SettingsPage.qml" line="13"/>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="13"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="27"/>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="27"/>
+        <source>Only in interal storage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="28"/>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="28"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="29"/>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="29"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="38"/>
         <location filename="../src/qml/pages/SettingsPage.qml" line="38"/>
         <source>Enable file sorting</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/pages/SettingsPage.qml" line="63"/>
         <location filename="../src/qml/pages/SettingsPage.qml" line="63"/>
         <source>Always play from the beginning</source>
         <translation type="unfinished"></translation>
@@ -63,6 +100,7 @@
     <name>TracksPage</name>
     <message>
         <location filename="../src/qml/pages/TracksPage.qml" line="16"/>
+        <location filename="../src/qml/pages/TracksPage.qml" line="16"/>
         <source>Tracks</source>
         <translation type="unfinished"></translation>
     </message>
@@ -70,22 +108,38 @@
 <context>
     <name>glacier-music</name>
     <message>
-        <location filename="../src/qml/glacier-music.qml" line="151"/>
+        <location filename="../src/qml/glacier-music.qml" line="133"/>
+        <location filename="../src/qml/glacier-music.qml" line="133"/>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/glacier-music.qml" line="134"/>
+        <location filename="../src/qml/glacier-music.qml" line="134"/>
+        <source>tags...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/glacier-music.qml" line="171"/>
+        <location filename="../src/qml/glacier-music.qml" line="171"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/glacier-music.qml" line="152"/>
+        <location filename="../src/qml/glacier-music.qml" line="172"/>
+        <location filename="../src/qml/glacier-music.qml" line="172"/>
         <source>Ok</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/glacier-music.qml" line="153"/>
+        <location filename="../src/qml/glacier-music.qml" line="173"/>
+        <location filename="../src/qml/glacier-music.qml" line="173"/>
         <source>No music files found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/glacier-music.qml" line="154"/>
+        <location filename="../src/qml/glacier-music.qml" line="174"/>
+        <location filename="../src/qml/glacier-music.qml" line="174"/>
         <source>Please add music files into Music or Download directory. Or connect card with music</source>
         <translation type="unfinished"></translation>
     </message>
