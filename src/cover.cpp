fa8c8a0ffdcf16cd5f0169d7a3c03a9e48bb004a
Sergey Chupligin <neochapay@gmail.com>
2022-04-29 14:53:53 +0300

    [cover] Create cover dir if not exist

--- src/cover.cpp
+++ src/cover.cpp
@@ -1,11 +1,33 @@
+/*
+ * Copyright (C) 2021-2022 Chupligin Sergey <neochapay@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include "cover.h"
 #include "audiofile.h"
 #include "musicbrainzconnect.h"
 
-
 Cover::Cover(QObject *parent) : QObject(parent)
+  ,m_coverDir(QStandardPaths::writableLocation(QStandardPaths::CacheLocation)+"/covers")
 {
-    m_coverDir = QStandardPaths::writableLocation(QStandardPaths::CacheLocation)+"/covers";;
+    QDir coverDir(m_coverDir);
+    if(!coverDir.exists()) {
+        coverDir.mkpath(m_coverDir);
+    }
 }
 
 void Cover::getCoverByTrackId(int trackId)
